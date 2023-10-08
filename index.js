7b9456ec87d9a705b191400f8115c9519ca8333a
Mehdi Faraji <m.faraji@shetab.net>
2023-10-08 22:32:48 +0330

    added post request for todo list

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get("/list", (req, res) => {
   res.send(list);
 });
 
+app.post("/list", async (req, res) => {
+  const todo = req.body;
+
+  list.list.push(todo);
+  fs.writeFile("list.json", JSON.stringify(list), () => {});
+  res.json(list);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
